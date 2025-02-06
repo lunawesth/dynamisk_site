fa936de2598cd24c1dbaf11b62155ec2232bba4a
Luna Westh <luwe0002@stud.kea.dk>
2025-02-06 12:00:02 +0100

    tilføjer alt

--- js/liste.js
+++ js/liste.js
@@ -2,8 +2,10 @@ console.log("script hentet");
 
 const category = "Apparel";
 const listContainer = document.querySelector(".productList");
+const params = new URLSearchParams(window.location.search);
+const selectedCategory = params.get("category");
 
-fetch(`https://kea-alt-del.dk/t7/api/products?limit=100`)
+fetch(`https://kea-alt-del.dk/t7/api/products?category=${selectedCategory}`)
   .then((response) => response.json())
   .then(showProductList);
 
@@ -12,10 +14,22 @@ function showProductList(data) {
     .map(
       (product) =>
         `
-      <section class="produkter">
-      <article>
-        <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="trøje">
-        <p><b>${product.productdisplayname}</b><br>DK ${product.price},- </p>
+      <section class="productList">
+      <article class="smallProduct ${
+        product.soldout ? "sold-out_container" : ""
+      }">
+        <img src="https://kea-alt-del.dk/t7/images/webp/640/${
+          product.id
+        }.webp" alt="trøje">
+        <p><b>${product.productdisplayname}</b><br>DKK ${product.price},- </p>
+        Nu DKK ${product.price},- ${
+          product.discount
+            ? `<span class="tilbud">${product.discount}%</span>`
+            : ""
+        }</p>
+
+        ${product.solout ? '<span class="sold-out_tekst">Udsolgt</span>' : ""}
+
         <a class="read_more" href="product.html">Gå til produkt</a>
       </article>
       </section>
